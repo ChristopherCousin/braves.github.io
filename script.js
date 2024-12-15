ad42fa4b3e4a5da61370bcbc77ae59fb5f22815e
Christopher C.A <tomasadison2014@gmail.com>
2024-12-15 15:58:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -686,7 +686,7 @@ function initializeStatistics() {
                 
                 // Animate each statistic with different delays
                 setTimeout(() => {
-                    animateValue(document.getElementById('userCount'), 0, 50000, 2000);
+                    animateValue(document.getElementById('userCount'), 0, 500, 2000);
                 }, 200);
                 
                 setTimeout(() => {
