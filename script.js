017cbd8f89ad30f269b4ba325a2162d48b1dc15d
Christopher C.A <tomasadison2014@gmail.com>
2025-02-05 21:07:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -341,9 +341,9 @@ document.addEventListener('DOMContentLoaded', function() {
         const observer = new IntersectionObserver((entries) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    animateValue("userCount", 0, 5652, 2000);
-                    animateValue("challengeCount", 0, 46, 2500);
-                    animateValue("gamesPlayed", 0, 76530, 3000);
+                    animateValue("userCount", 0, 13652, 2000);
+                    animateValue("challengeCount", 0, 77, 2500);
+                    animateValue("gamesPlayed", 0, 136530, 3000);
                     observer.unobserve(entry.target);
                 }
             });
